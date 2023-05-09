3a4d918785f6f43201599ff57137e809fac4a47b
Paul <57140776+Pawl-Patrol@users.noreply.github.com>
2023-05-09 18:32:24 +0200

    graphics go brrrrrr

--- src/gpu.rs
+++ src/gpu.rs
@@ -14,12 +14,31 @@ pub const COLOR_BLACK: u32 = rgb!(0x00, 0x00, 0x00);
 pub const SCREEN_WIDTH: usize = 160;
 pub const SCREEN_HEIGHT: usize = 144;
 
-pub const SCANLINE_CYCLES: u16 = 456;
+pub const MODE_HBLANK: u8 = 0b00;
+pub const MODE_VBLANK: u8 = 0b01;
+pub const MODE_OAM: u8 = 0b10;
+pub const MODE_VRAM: u8 = 0b11;
+
+type Tile = [[u8; 8]; 8];
+
+#[derive(Default, Copy, Clone)]
+pub struct Sprite {
+    y: u8,
+    x: u8,
+    tile_index: u8,
+    bg_priority: bool,
+    y_flip: bool,
+    x_flip: bool,
+    palette: bool,
+}
 
 pub struct GPU {
-    pub vram: [u8; 0x2000], // video ram
-    pub oam: [u8; 0xA0],    // object attribute memory
-    pub video_buffer: Vec<u32>,
+    pub vram: [u8; 0x2000],    // 8KB of video ram
+    pub oam: [u8; 160],        // 160 bytes of sprite attribute memory
+    pub tiles: [Tile; 384],    // 384 tiles, each tile is 8x8 pixels
+    pub sprites: [Sprite; 40], // 40 sprites
+    pub video_buffer: [u32; SCREEN_WIDTH * SCREEN_HEIGHT],
+    pub cycles: u16,
     pub scanline_counter: u16,
     pub lcd_control: u8,
     pub lcd_status: u8,
@@ -30,8 +49,8 @@ pub struct GPU {
     pub ly: u8,
     pub ly_compare: u8,
     pub bg_palette: u8,
+    pub obj_palette_0: u8,
     pub obj_palette_1: u8,
-    pub obj_palette_2: u8,
 }
 
 impl Cartridge for GPU {
@@ -46,8 +65,8 @@ impl Cartridge for GPU {
             0xFF44 => self.ly,
             0xFF45 => self.ly_compare,
             0xFF47 => self.bg_palette,
-            0xFF48 => self.obj_palette_1,
-            0xFF49 => self.obj_palette_2,
+            0xFF48 => self.obj_palette_0,
+            0xFF49 => self.obj_palette_1,
             0xFF4A => self.window_y,
             0xFF4B => self.window_x,
             _ => {
@@ -59,8 +78,20 @@ impl Cartridge for GPU {
 
     fn write(&mut self, address: usize, value: u8) {
         match address {
-            0x8000..=0x9FFF => self.vram[address - 0x8000] = value,
-            0xFE00..=0xFE9F => self.oam[address - 0xFE00] = value,
+            0x8000..=0x9FFF => {
+                let address = address - 0x8000;
+                self.vram[address] = value;
+                if address < 0x1800 {
+                    self.update_tile(address);
+                }
+            }
+            0xFE00..=0xFE9F => {
+                let address = address - 0xFE00;
+                self.oam[address] = value;
+                if address < 0xA0 {
+                    self.update_sprite(address);
+                }
+            }
             0xFF40 => self.lcd_control = value,
             0xFF41 => self.lcd_status = value,
             0xFF42 => self.scroll_y = value,
@@ -68,8 +99,8 @@ impl Cartridge for GPU {
             0xFF44 => self.ly = 0,
             0xFF45 => self.ly_compare = value,
             0xFF47 => self.bg_palette = value,
-            0xFF48 => self.obj_palette_1 = value,
-            0xFF49 => self.obj_palette_2 = value,
+            0xFF48 => self.obj_palette_0 = value,
+            0xFF49 => self.obj_palette_1 = value,
             0xFF4A => self.window_y = value,
             0xFF4B => self.window_x = value,
             _ => (), // println!("Invalid gpu address: 0x{:04X} = {:02X}", address, value),
@@ -81,8 +112,11 @@ impl GPU {
     pub fn new() -> GPU {
         GPU {
             vram: [0; 0x2000],
-            oam: [0; 0xA0],
-            video_buffer: vec![0; SCREEN_WIDTH * SCREEN_HEIGHT],
+            oam: [0; 160],
+            sprites: [Sprite::default(); 40],
+            tiles: [[[0; 8]; 8]; 384],
+            video_buffer: [0; SCREEN_WIDTH * SCREEN_HEIGHT],
+            cycles: 0,
             scanline_counter: 0,
             lcd_control: 0x91,
             lcd_status: 0x85,
@@ -91,111 +125,151 @@ impl GPU {
             ly: 0x00,
             ly_compare: 0x00,
             bg_palette: 0xFC,
+            obj_palette_0: 0xFF,
             obj_palette_1: 0xFF,
-            obj_palette_2: 0xFF,
             window_y: 0x00,
             window_x: 0x00,
         }
     }
+
     pub fn update_graphics(&mut self, cycles: u16) -> u8 {
-        let mut needs_interrupt = self.set_lcd_status();
+        let mut needs_interrupt = 0;
 
         if !self.lcd_enabled() {
-            return 0;
+            return needs_interrupt;
         }
 
-        if let Some(new_scanline_counter) = self.scanline_counter.checked_sub(cycles) {
-            self.scanline_counter = new_scanline_counter;
-        } else {
-            self.scanline_counter = SCANLINE_CYCLES;
-            self.ly += 1;
+        self.cycles += cycles;
 
-            if self.ly == 144 {
-                needs_interrupt |= 1;
-            } else if self.ly > 153 {
-                self.ly = 0;
-            } else if self.ly < 144 {
-                self.draw_scanline();
+        match self.mode() {
+            MODE_HBLANK => {
+                if self.cycles > 200 {
+                    self.cycles -= 200;
+                    self.ly += 1;
+                    if self.ly >= 144 {
+                        self.set_mode(MODE_VBLANK);
+                        needs_interrupt |= 1; // VBLANK interrupt
+                        if self.lcd_status.test_bit(4) {
+                            needs_interrupt |= 2; // LCD STAT interrupt
+                        }
+                    } else {
+                        self.set_mode(MODE_OAM);
+                        if self.lcd_status.test_bit(5) {
+                            needs_interrupt |= 2; // LCD STAT interrupt
+                        }
+                    }
+                    needs_interrupt |= self.compare_ly_lyc();
+                }
             }
+            MODE_VBLANK => {
+                if self.cycles >= 456 {
+                    self.cycles %= 456;
+                    self.ly += 1;
+                    if self.ly == 154 {
+                        self.ly = 0;
+                        self.set_mode(MODE_OAM);
+                        if self.lcd_status.test_bit(5) {
+                            needs_interrupt |= 2; // LCD STAT interrupt
+                        }
+                    }
+                    needs_interrupt |= self.compare_ly_lyc();
+                }
+            }
+            MODE_OAM => {
+                if self.cycles >= 80 {
+                    self.cycles %= 80;
+                    self.set_mode(MODE_VRAM);
+                }
+            }
+            MODE_VRAM => {
+                if self.cycles >= 172 {
+                    self.cycles %= 172;
+                    self.set_mode(MODE_HBLANK);
+                    if self.lcd_status.test_bit(3) {
+                        needs_interrupt |= 2; // LCD STAT interrupt
+                    }
+                    self.render_scanline();
+                }
+            }
+            _ => unreachable!(),
         }
 
         needs_interrupt
     }
 
-    fn set_lcd_status(&mut self) -> u8 {
-        if !self.lcd_enabled() {
-            self.scanline_counter = SCANLINE_CYCLES;
-            self.ly = 0;
-            self.lcd_status &= 0b1111_1100;
-            self.lcd_status.set_bit(0);
-            return 0;
+    fn get_color(palette: u8, color: u8) -> u32 {
+        match (palette >> (color * 2)) & 0b11 {
+            0b00 => COLOR_WHITE,
+            0b01 => COLOR_LIGHT_GRAY,
+            0b10 => COLOR_DARK_GRAY,
+            0b11 => COLOR_BLACK,
+            _ => unreachable!(),
         }
+    }
 
-        let current_mode = self.lcd_status & 0b0000_0011;
-        let mode;
-        let mut request_interrupt = false;
+    fn compare_ly_lyc(&mut self) -> u8 {
+        let result = self.ly == self.ly_compare;
+        self.lcd_status.toggle_bit(2, result); // set ly=lyc flag
 
-        if self.ly >= 144 {
-            mode = 1;
-            self.lcd_status.set_bit(0);
-            self.lcd_status.reset_bit(1);
-            request_interrupt = self.lcd_status.test_bit(4);
+        // if ly=lyc interrupt is enabled
+        if result && self.lcd_status.test_bit(6) {
+            2 // LCD STAT interrupt
         } else {
-            let mode2_bounds = SCANLINE_CYCLES - 80;
-            let mode3_bounds = mode2_bounds - 172;
-
-            if self.scanline_counter >= mode2_bounds {
-                mode = 2;
-                self.lcd_status.set_bit(1);
-                self.lcd_status.reset_bit(0);
-                request_interrupt = self.lcd_status.test_bit(5);
-            } else if self.scanline_counter >= mode3_bounds {
-                mode = 3;
-                self.lcd_status.set_bit(1);
-                self.lcd_status.set_bit(0);
-            } else {
-                mode = 0;
-                self.lcd_status.reset_bit(1);
-                self.lcd_status.reset_bit(0);
-                request_interrupt = self.lcd_status.test_bit(3);
-            }
+            0
         }
+    }
 
-        let mut interrupt_flag = 0;
+    fn mode(&self) -> u8 {
+        self.lcd_status & 0b11
+    }
 
-        if request_interrupt && (mode != current_mode) {
-            interrupt_flag |= 1 << 1;
-        }
-
-        if self.ly == self.ly_compare {
-            self.lcd_status.set_bit(2);
-            if self.lcd_status.test_bit(6) {
-                interrupt_flag |= 1 << 1;
-            }
-        } else {
-            self.lcd_status.reset_bit(2);
-        }
-
-        interrupt_flag
+    fn set_mode(&mut self, mode: u8) {
+        self.lcd_status = (self.lcd_status & 0b1111_1100) | mode;
     }
 
     fn lcd_enabled(&self) -> bool {
         self.lcd_control.test_bit(7)
     }
 
-    fn draw_scanline(&mut self) {
+    fn update_tile(&mut self, address: usize) {
+        let normalized = address & 0xFFFE; // round even
+        let data1 = self.vram[normalized];
+        let data2 = self.vram[normalized + 1];
+        let tile = address / 16;
+        let y = (address % 16) / 2;
+        for x in 0..8 {
+            let bit1 = (data1 >> (7 - x)) & 0b1;
+            let bit2 = (data2 >> (7 - x)) & 0b1;
+            let color = (bit2 << 1) | bit1;
+            self.tiles[tile][y][x] = color;
+        }
+    }
+
+    fn update_sprite(&mut self, address: usize) {
+        let address = address & 0xFFFC; // round to multiple of 4
+        let sprite = address / 4;
+        self.sprites[sprite].y = self.oam[address].wrapping_sub(16);
+        self.sprites[sprite].x = self.oam[address + 1].wrapping_sub(8);
+        self.sprites[sprite].tile_index = self.oam[address + 2];
+        let flags = self.oam[address + 3];
+        self.sprites[sprite].bg_priority = flags.test_bit(7);
+        self.sprites[sprite].y_flip = flags.test_bit(6);
+        self.sprites[sprite].x_flip = flags.test_bit(5);
+        self.sprites[sprite].palette = flags.test_bit(4);
+    }
+
+    fn render_scanline(&mut self) {
         if self.lcd_control.test_bit(0) {
-            self.render_tiles()
+            self.render_tiles();
         }
         if self.lcd_control.test_bit(1) {
-            self.render_sprites()
+            self.render_sprites();
         }
     }
 
     fn render_tiles(&mut self) {
-        let using_window = self.lcd_control.test_bit(5) && (self.window_y <= self.ly);
+        let using_window = self.lcd_control.test_bit(5) && self.window_y <= self.ly;
         let unsigned = self.lcd_control.test_bit(4);
-        let tile_data = if unsigned { 0x8000 } else { 0x8800 };
 
         let background_mem = if using_window {
             if self.lcd_control.test_bit(6) {
@@ -211,130 +285,73 @@ impl GPU {
             }
         };
 
-        let y_pos = if using_window {
+        let y = if using_window {
             self.ly.wrapping_sub(self.window_y)
         } else {
-            self.scroll_y.wrapping_add(self.ly)
+            self.ly.wrapping_add(self.scroll_y)
         };
 
-        let tile_row = ((y_pos / 8) as u16) * 32;
+        let tile_row = (y / 8) as u16 * 32;
+
+        let corrected_window_x = self.window_x.wrapping_sub(7);
 
         for pixel in 0_u8..160_u8 {
-            let x_pos = pixel.wrapping_add(self.scroll_x);
-
-            let tile_address = background_mem + tile_row + (x_pos / 8) as u16;
-            let tile_num = self.read(tile_address as usize);
-            let tile_location = if unsigned {
-                tile_data + (tile_num as u16 * 16)
+            let x = if using_window && pixel >= corrected_window_x {
+                pixel.wrapping_sub(corrected_window_x)
             } else {
-                tile_data + ((tile_num as i8 as i16 + 128) as u16 * 16)
+                pixel.wrapping_add(self.scroll_x)
+            };
+            let tile_col = (x / 8) as u16;
+            let tile_address = background_mem + tile_row + tile_col;
+            let tile_offset = self.read(tile_address as usize);
+            let tile_index = if unsigned {
+                tile_offset as usize
+            } else {
+                tile_offset.wrapping_add(128) as usize + 128
             };
 
-            let line = ((y_pos % 8) * 2) as u16;
-            let data1 = self.read((tile_location + line) as usize);
-            let data2 = self.read((tile_location + line + 1) as usize);
+            let tile_color_index = self.tiles[tile_index][y as usize % 8][x as usize % 8];
+            let color = GPU::get_color(self.bg_palette, tile_color_index);
 
-            let color_bit = 7 - (x_pos % 8);
-            let color_num =
-                ((data2.test_bit(color_bit) as u8) << 1) | (data1.test_bit(color_bit) as u8);
-            let color = self.get_color(color_num, 0xFF47);
-
-            if self.ly > 143 || pixel > 159 {
-                continue;
-            }
-
-            self.video_buffer[self.ly as usize * SCREEN_WIDTH + pixel as usize] = color;
+            self.video_buffer[self.ly as usize * 160 + pixel as usize] = color;
         }
     }
 
     fn render_sprites(&mut self) {
         let using_8x16 = self.lcd_control.test_bit(2);
 
-        for sprite in 0..40 {
-            let index = 0xFE00 + sprite as usize * 4;
-
-            let y_pos = self.read(index).wrapping_sub(16);
-            let x_pos = self.read(index + 1).wrapping_sub(8);
-
-            let tile_location = self.read(index + 2);
-            let attributes = self.read(index + 3);
-
-            let y_flip = attributes.test_bit(6);
-            let x_flip = attributes.test_bit(5);
-
-            let y_size = if using_8x16 { 16 } else { 8 };
-
-            if (self.ly < y_pos) || (self.ly >= y_pos + y_size) {
+        for sprite in &self.sprites {
+            let size_y = if using_8x16 { 16 } else { 8 };
+            if self.ly < sprite.y || self.ly >= sprite.y + size_y {
                 continue;
             }
-
-            let mut line = self.ly - y_pos;
-
-            if y_flip {
-                line = y_size - line;
+            let mut y = (self.ly - sprite.y) % 8; // modulo 8 because of 8x16 sprites
+            if sprite.y_flip {
+                y = 7 - y;
             }
 
-            line *= 2;
+            let tile = self.tiles[sprite.tile_index as usize];
 
-            let address = 0x8000 + (tile_location as u16 * 16) + line as u16;
-
-            let data1 = self.read(address as usize);
-            let data2 = self.read(address as usize + 1);
-
-            for tile_pixel in (0_u8..8_u8).rev() {
-                let mut color_bit = tile_pixel;
-                if x_flip {
-                    color_bit = 7 - color_bit;
+            for pixel in 0..8 {
+                let mut x = pixel;
+                if sprite.x_flip {
+                    x = 7 - x;
                 }
-                let color_num =
-                    ((data2.test_bit(color_bit) as u8) << 1) | (data1.test_bit(color_bit) as u8);
-                let color_address = if attributes.test_bit(4) {
-                    0xFF49
+                let color_index = tile[y as usize][x as usize];
+                if color_index == 0 {
+                    continue;
+                }
+                let palette = if sprite.palette {
+                    self.obj_palette_1
                 } else {
-                    0xFF48
+                    self.obj_palette_0
                 };
-
-                let color = self.get_color(color_num, color_address);
-
-                if color == COLOR_WHITE {
-                    continue;
+                let color = GPU::get_color(palette, color_index);
+                let index = self.ly as usize * 160 + sprite.x.wrapping_add(pixel) as usize;
+                if !sprite.bg_priority || self.video_buffer[index] == COLOR_WHITE {
+                    self.video_buffer[index] = color;
                 }
-
-                let pixel = x_pos.wrapping_add(7).wrapping_sub(tile_pixel);
-
-                if self.ly > 143 || pixel > 159 {
-                    continue;
-                }
-
-                let video_buffer_index = self.ly as usize * SCREEN_WIDTH + pixel as usize;
-
-                if attributes.test_bit(7) && self.video_buffer[video_buffer_index] != COLOR_WHITE {
-                    continue;
-                }
-
-                self.video_buffer[video_buffer_index] = color;
             }
-        }
-    }
-
-    fn get_color(&self, color_num: u8, address: u16) -> u32 {
-        let palette = self.read(address as usize);
-        let (hi, lo) = match color_num {
-            0 => (1, 0),
-            1 => (3, 2),
-            2 => (5, 4),
-            3 => (7, 6),
-            _ => panic!("Invalid color number"),
-        };
-
-        let color = ((palette.test_bit(hi) as u8) << 1) | palette.test_bit(lo) as u8;
-
-        match color {
-            0 => COLOR_WHITE,
-            1 => COLOR_LIGHT_GRAY,
-            2 => COLOR_DARK_GRAY,
-            3 => COLOR_BLACK,
-            _ => panic!("Invalid color"),
         }
     }
 }
